1a67094182c0eb27f43e0e9eb785ec2c0d83ded4
raffless007 <rsiddiquey@gmail.com>
2026-07-10 02:17:05 +1000

    Delete 006_badminton_score_format.sql

--- 006_badminton_score_format.sql
+++
@@ -1,9 +0,0 @@
--- Update score constraints for badminton points.
-
-alter table public.match_scores
-  drop constraint if exists match_scores_games_a_check,
-  drop constraint if exists match_scores_games_b_check;
-
-alter table public.match_scores
-  add constraint match_scores_games_a_check check (games_a between 0 and 30),
-  add constraint match_scores_games_b_check check (games_b between 0 and 30);
